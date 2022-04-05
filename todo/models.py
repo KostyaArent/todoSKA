2610d44b06072949f9b03e3119a8dbcb42acbcac
KostyaArent <trepalin.konstantin@gmail.com>
2022-04-05 19:53:05 +0000

    todo model added

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    STATUSES_CHOISES = [
+        ('IN_PROGRESS', 'In progress'),
+        ('COMPLITED', 'Completed'),
+        ('FAILED', 'Failed'),
+        ('NOT_STARTED', 'Not started'),
+    ]
+    PRIORITIES_CHOISES = [
+        ('1', 'LOW'),
+        ('2', 'MEDIUM'),
+        ('3', 'HIGH'),
+    ]
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    executor = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=15, choices=STATUSES_CHOISES, default='NOT_STARTED')
+    priority = models.CharField(max_length=15,choices=PRIORITIES_CHOISES, default='LOW')
+    created_date = models.DateTimeField(auto_now_add=True)
+    deadline_date = models.DateTimeField(null=True, blank=True)
+    close_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+            return self.title

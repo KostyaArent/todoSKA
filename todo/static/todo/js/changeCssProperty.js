962027e305878dc32b8dc7877ad12985595d13b0
KostyaArent <trepalin.konstantin@gmail.com>
2022-04-09 12:06:05 +0000

    little style 2

---
+++ todo/static/todo/js/changeCssProperty.js
@@ -0,0 +1,15 @@
+function getCssProperty(elmId, property){
+   var elem = document.getElementById(elmId);
+   return window.getComputedStyle(elem,null).getPropertyValue(property);
+};
+
+
+function widthUpdate(elmId, propernyName, propertyValue) {
+    var elem = document.getElementById(elmId);
+    elem.style.setProperty(propernyName, propertyValue)
+};
+
+$(document).ready(function () {
+  var contentWidth = getCssProperty("contentContainer", "height");
+  widthUpdate("backgroundContainer", "height", contentWidth);
+});
